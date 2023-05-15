91f478a987bad90d005d5d8e8ad573724f425eec
amiel <1633827057@qq.com>
2023-05-15 14:04:28 +0800

    合并聊天系统

---
+++ src/main/java/com/example/renwushu/timers/MinioTimer.java
@@ -0,0 +1,14 @@
+package com.example.renwushu.timers;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MinioTimer {
+
+//    @Scheduled(cron = "0 0 1 * * ?")
+//    public  void clearFile(){
+//
+//    }
+
+
+}
